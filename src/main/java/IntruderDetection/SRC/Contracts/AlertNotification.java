3c4af1a4b54b9819215f18f20a8bd0e2b7669ea6
Tookbaegee <top1236951@gmail.com>
2022-04-24 15:18:50 -0700

    Adds camera image handling

--- src/main/java/IntruderDetection/SRC/Contracts/AlertNotification.java
+++ src/main/java/IntruderDetection/SRC/Contracts/AlertNotification.java
@@ -1,20 +1,27 @@
 package IntruderDetection.SRC.Contracts;
 
+import java.awt.*;
+
 public class AlertNotification extends Notification{
-    private byte[][] cameraStream;
+    private Image cameraStream;
     private boolean alertRaised = false;
 
-    public AlertNotification(byte[][] cameraStream, boolean alertRaised){
+    private Integer round;
+
+    public AlertNotification(Image cameraStream, boolean alertRaised, Integer round){
 
         this.cameraStream = cameraStream;
         this.alertRaised = alertRaised;
+        this.round = round;
     }
 
-    public byte[][] getCameraStream() {
+    public Image getCameraStream() {
         return cameraStream;
     }
 
     public boolean isAlertRaised() {
         return alertRaised;
     }
+
+    public Integer getRound() { return round; }
 }
--- src/main/java/IntruderDetection/SRC/NotificationHandlers/AlertNotificationHandler.java
+++ src/main/java/IntruderDetection/SRC/NotificationHandlers/AlertNotificationHandler.java
@@ -4,14 +4,24 @@ import IntruderDetection.SRC.Contracts.AlertNotification;
 import IntruderDetection.SRC.Contracts.INotificationHandler;
 import IntruderDetection.SRC.Contracts.Notification;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class AlertNotificationHandler implements INotificationHandler {
     @Override
     public void notify(Notification notification) {
         AlertNotification alertNotification = (AlertNotification) notification;
-
         if(alertNotification.isAlertRaised()) {
             System.out.println("The user has been alerted about the intruder");
             System.out.println("The image of intruder is captured and sent to the intruder");
+            File outputFile = new File("imageNotifications/" + ((AlertNotification) notification).getRound() + ".jpg");
+            try {
+                ImageIO.write((BufferedImage)((AlertNotification) notification).getCameraStream(), "jpg", outputFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } else {
             System.out.println("The intruder is not in the sur.veil·lance");
             System.out.println("The user is not notified about any alert");
