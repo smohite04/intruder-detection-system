034a6d56c8bc4fee2f1e5f70a0a0698488b557f9
Tookbaegee <top1236951@gmail.com>
2022-04-24 21:11:12 -0700

    fix: audio alarm

--- src/main/java/IntruderDetection/SRC/AlarmNotification.java
+++ src/main/java/IntruderDetection/SRC/AlarmNotification.java
@@ -20,23 +20,13 @@ public class AlarmNotification {
 
 	public AlarmNotification(String filepath) {
 		this.filepath = filepath;
-	}
-
-	public void startAlarmProcess(boolean alarm) {
+		this.statusClip = "paused";
 
 		try {
-			this.alarm = alarm;
 			System.out.println("Deciding whether alarm should be triggered or not");
 			audioInputStream = AudioSystem.getAudioInputStream(new File(this.filepath));
 			clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
-
-			//clip.loop(Clip.LOOP_CONTINUOUSLY);
-			if (alarm == true) {
-				play();
-			} else {
-				pause();
-			}
 		} catch (UnsupportedAudioFileException e1) {
 			System.out.println("UnSupported Audio File Exception has occured");
 			e1.printStackTrace();
@@ -47,14 +37,22 @@ public class AlarmNotification {
 			System.out.println("LineUnavailable Exception has occured");
 			e3.printStackTrace();
 		}
+	}
 
+	public void startAlarmProcess(boolean alarm) {
+		this.alarm = alarm;
+		if (alarm) {
+			play();
+		} else {
+			pause();
+		}
 	}
 
 	public void play() {
-
 		if ( statusClip!=null && statusClip.equals("play")) {
 			System.out.println("The alarm is already on");
 		} else {
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
 			clip.start();
 			statusClip = "play";
 			System.out.println("The Alarm is triggered");
@@ -64,10 +62,12 @@ public class AlarmNotification {
 	public void pause() {
 		if(statusClip.equals("paused")) {
 			System.out.println("The alarm is already paused");
+		} else {
+			clip.stop();
+			statusClip="paused";
+			System.out.println("The alarm is turned off as intruder is moving away from proximity area");
 		}
-		clip.stop();
-		statusClip="paused";
-		System.out.println("The alarm is turned off as intruder is moving away from proximity area");
+
 	}
 
 	public boolean isAlarm() {
